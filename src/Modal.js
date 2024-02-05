c7c54371f53975895181aa63dfdffe84473a0c61
Jaiko657 <matthewboyd03@gmail.com>
2024-02-05 16:29:37 +0000

    increase claim quality

--- src/Modal.js
+++ src/Modal.js
@@ -12,6 +12,7 @@ const Modal = ({ showModal, toggleModal, type }) => {
       left: 0,
       right: 0,
       bottom: 0,
+      padding: '2vw',
       backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background
       display: 'flex',
       alignItems: 'center',
@@ -22,13 +23,18 @@ const Modal = ({ showModal, toggleModal, type }) => {
         // backgroundColor: '#152C12',
         height: '50vh',
         width: '80vw',
+        marginRight: '2vw',
+        marginLeft: '2vw',
         boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
         display: 'flex',
         // flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center'
       }}>
-        <img src={claim} onDoubleClick={toggleModal}></img>
+        <img src={claim} style={{
+          width: '98vw',
+          height: '47vh'
+        }}></img>
       </div>
     </div>
   );
